f547986c3502c38e2f177eeeb0a7016b7f514668
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-04-02 17:01:23 -0500

    Add Chance usage to create random userID numbers

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 'use strict';
 
 // let eventPool = require('./eventPool');
+let Chance = require('chance');
 
-// event handlers
-// const driverHandler = require('./driver');
-// const vendorHandler = require('./vendor');
+var chance = new Chance();
 
-// listen to all events
+// // event handlers
+// const driverHandler = require('./src/driver');
+// const vendorHandler = require('./src/vendor');
+
+// // listen to all events
 // eventPool.on('PICKUP', driverHandler);
 // eventPool.on('DELIVERED', vendorHandler);
 
 setInterval(() => {
 
   console.log('---------------new inerval begins-------------');
+  let randomID = chance.guid();
+  console.log('Random userID: ', randomID);
 
 }, 5000);
