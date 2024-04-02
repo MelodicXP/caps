a0182c44b545b3561adb79d48fe02eaf4230b326
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-04-02 16:51:29 -0500

    Test proof of life on interval timer

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// let eventPool = require('./eventPool');
+
+// event handlers
+// const driverHandler = require('./driver');
+// const vendorHandler = require('./vendor');
+
+// listen to all events
+// eventPool.on('PICKUP', driverHandler);
+// eventPool.on('DELIVERED', vendorHandler);
+
+setInterval(() => {
+
+  console.log('---------------new inerval begins-------------');
+
+}, 5000);
