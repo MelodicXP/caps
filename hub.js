04a925fc3ce0f8b8c93fd454760e71a80e10b2db
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-04-03 08:56:03 -0500

    Refactor code

--- hub.js
+++ hub.js
@@ -1,10 +1,10 @@
 'use strict';
 
 // let eventPool = require('./eventPool');
-let Chance = require('chance');
+
 const eventPool = require('./eventPool');
 
-var chance = new Chance();
+
 
 // event handlers
 const driverHandler = require('./driver/index');
@@ -13,12 +13,3 @@ const vendorHandler = require('./vendor/index');
 // listen to all events
 eventPool.on('PICKUP', driverHandler);
 eventPool.on('DELIVERED', vendorHandler);
-
-setInterval(() => {
-
-  console.log('---------------new inerval begins-------------');
-  const productID = chance.guid();
-  console.log('Product ID: ', productID);
-  eventPool.emit('PICKUP', { productID });
-
-}, 10000);
--- vendor/index.js
+++ vendor/index.js
@@ -1,5 +1,20 @@
 'use strict';
 
+const eventPool = require('../eventPool');
+
+let Chance = require('chance');
+var chance = new Chance();
+
+setInterval(() => {
+
+  console.log('---------------new inerval begins-------------');
+  const productID = chance.guid();
+  console.log('Product ID: ', productID);
+  eventPool.emit('PICKUP', { productID });
+
+}, 10000);
+
+
 module.exports = (payload) => {
   setTimeout(() => {
     console.log(`Product ${payload.productID} has been delivered.`);
