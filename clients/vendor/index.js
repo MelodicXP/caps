7a755aaef118625b95592aac4b7bdba1e907fda4
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-04-10 09:18:04 -0500

    Update room name

--- clients/vendor/index.js
+++ clients/vendor/index.js
@@ -5,6 +5,6 @@ const triggerOrderReadyForPickup = require('../vendor/packageReadyForPickup');
 
 const socket = io('http://localhost:3001/caps');
 
-socket.emit('JOIN', 'caps');
+socket.emit('JOIN', 'caps-room');
 
 triggerOrderReadyForPickup('1-206-flowers');
