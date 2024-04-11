6996abe4a03ebdd9c4ea927cfb32c3d7f123b52d
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-04-11 10:36:59 -0500

    Refactor to handle name specific broadcasting of events

--- clients/eventEmitter.js
+++ clients/eventEmitter.js
@@ -1,22 +1,38 @@
 'use strict';
 
-// const { io } = require('socket.io-client');
-// const socket = io('http://localhost:3001/caps');
-
 const emit = {
-  eventAndPayload: (socket, eventName, incomingPayload) => {
-    if(incomingPayload.event && incomingPayload.payload) {
+  eventAndPayload: function (socket, eventName, incomingPayload) {
+    let eventObject = incomingPayload;
+
+    if (!incomingPayload.event || !incomingPayload.payload) {
+      // Constructs a new event object if incomingPayload does not already follow the structure
+      eventObject = this.createEventObject(eventName, incomingPayload);
+    } else {
+      // Update existing event name and date
       incomingPayload.event = eventName.toLowerCase();
       incomingPayload.time = new Date().toISOString();
-      socket.emit(eventName.toUpperCase(), incomingPayload);
-    } else {
-      const event = {
-        event: eventName.toLowerCase(),
-        time: new Date().toISOString(),
-        payload: incomingPayload,
-      };
-      socket.emit(eventName.toUpperCase(), event);
     }
+    this.emitEvent(socket, eventName, eventObject, incomingPayload);
+  },
+
+  createEventObject(eventName, payload) {
+    return {
+      event: eventName.toLowerCase(),
+      time: new Date().toISOString(),
+      payload,
+    };
+  },
+
+  emitEvent(socket, eventName, eventObject, payload) {
+    if (this.shouldEmitToRoom(eventName, payload)) {
+      socket.to(payload.vendorRoom).emit(eventName.toUpperCase(), eventObject);
+    } else {
+      socket.emit(eventName.toUpperCase(), eventObject);
+    }
+  },
+
+  shouldEmitToRoom(eventName, payload) {
+    return ['IN_TRANSIT', 'DELIVERED'].includes(eventName.toUpperCase()) && payload.vendorRoom;
   },
 };
 
