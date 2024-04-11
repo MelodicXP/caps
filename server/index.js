f072d72521f15cc237b7ad10904f80619e816512
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-04-11 10:42:18 -0500

    Add ability to broadcast to specific room by vendorRoom

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3002;
 const server = new Server();
 server.listen(PORT);
 
-// server space
+// default server space
 server.on('connection', (socket) => {
   console.log('Socket connected to Event Server!', socket.id);
   
@@ -23,34 +23,40 @@ server.on('connection', (socket) => {
   });
 });
 
-// caps namespace
-// listening for events at http://localhost:3001/caps
+// Define 'caps' name space
+// Listening for events at http://localhost:3001/caps
 const caps = server.of('/caps');
 
 caps.on('connection', (socket) => {
   console.log('Socket connected to caps namespace!', socket.id);
 
-  // how to join room
+  // Handle joining rooms
   socket.on('JOIN', (room) => {
     console.log('these are the rooms', socket.adapter.rooms);
     console.log('---payload is the room---', room);
     socket.join(room);
-    console.log(`You've joined the ${room} room`);
+    console.log(`Socket ${socket.id} joined room: ${room}`);
   });
 
-  // Listening to log events and transmit to recipients
+  // Handle PICKUP event (global broadcast within namespace, except the sender)
   socket.on('PICKUP', (payload) => {
-    console.log('EVENT: ', payload);
+    console.log('PICKUP EVENT: ', payload);
     socket.broadcast.emit('PICKUP', payload);
   });
 
+  // Handle IN_TRANSIT event (emit only to specific room, typically vendor's room)
   socket.on('IN_TRANSIT', (payload) => {
-    console.log('EVENT: ', payload);
-    socket.broadcast.emit('IN_TRANSIT', payload);
+    console.log('IN_TRANSIT EVENT: ', payload);
+    if(payload.vendorRoom) {
+      socket.to(payload.vendorRoom).emit('IN_TRANSIT', payload);
+    }
   });
 
+  // Handle DELIVERED event (similar to IN_TRANSIT, targeted to specific room)
   socket.on('DELIVERED', (payload) => {
-    console.log('EVENT: ', payload);
-    socket.broadcast.emit('DELIVERED', payload);
+    console.log('DELIVERED EVENT: ', payload);
+    if (payload.payload.vendorRoom) {
+      socket.to(payload.payload.vendorRoom).emit('DELIVERED', payload);
+    }
   });
 });
